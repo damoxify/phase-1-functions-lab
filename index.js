bd33ad6f62cb719aad56c01c56c2e0884c73b16c
Adedamola Ajasa <azeemajasa@gmail.com>
2023-07-30 12:44:07 +0100

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupLocation) {
+    const hqLocation = 42; 
+    return Math.abs(pickupLocation - hqLocation); 
+  }
+
+  function distanceFromHqInFeet(pickupLocation) {
+    const blocks = distanceFromHqInBlocks(pickupLocation);
+    const feetInBlock = 264; 
+    return blocks * feetInBlock;
+  }
+
+  function distanceTravelledInFeet(start, destination) {
+    const blocksTravelled = Math.abs(destination - start);
+    const feetInBlock = 264; 
+    return blocksTravelled * feetInBlock;
+  }
+
+  function calculatesFarePrice(start, destination) {
+    const distance = distanceTravelledInFeet(start, destination);
+  
+    if (distance <= 400) {
+      return 0; 
+    } else if (distance > 400 && distance <= 2000) {
+      return (distance - 400) * 0.02;
+    } else if (distance > 2000 && distance <= 2500) {
+      return 25;
+    } else {
+      return 'cannot travel that far'; 
+    }
+  }
